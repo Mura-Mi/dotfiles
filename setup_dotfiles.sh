4c344299971698d64a436c3862f67d9ec9d0d4df
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2018-04-01 21:22:21 +0900

    Remove bash profiles

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -6,14 +6,6 @@ if [ ! -e ~/.vimrc ]; then
   ln -s ~/dotfiles/.vimrc ~/.vimrc
 fi
 
-if [ -e ~/.bashrc ]; then
-  rm ~/.bashrc
-fi
-
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-rm ~/.bash_profile
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-
 if [ ! -e ~/.gitconfig ]; then
   ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 fi
