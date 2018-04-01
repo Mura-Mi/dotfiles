ebb43962d67e58ebf8dc2b3153cb5d2f45f10fe1
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2018-04-01 21:24:22 +0900

    Suppress warnings of creating symbolic link failure when symbolic link already exists

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -2,21 +2,21 @@
 
 cd `dirname $0`
 
-if [ ! -e ~/.vimrc ]; then
+if [ ! -e ~/.vimrc -o ! -L ~/.vimrc ]; then
   ln -s ~/dotfiles/.vimrc ~/.vimrc
 fi
 
-if [ ! -e ~/.gitconfig ]; then
+if [ ! -e ~/.gitconfig -o ! -L ~/.gitconfig ]; then
   ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 fi
 
-if [ ! -e ~/.zshrc ]; then
+if [ ! -e ~/.zshrc -o ! -L ~/.zshrc ]; then
   ln -s ~/dotfiles/.zshrc ~/.zshrc
 fi
 
 cp -r ./.sbt $HOME/.sbt
   
-if [ ! -e ~/.sbtrc ]; then
-  ln -s ./.sbtrc $HOME/
+if [ ! -e ~/.sbtrc -o ! -L ~/.sbtrc ]; then
+  ln -s ~/dotfiles/.sbtrc ~/.sbtrc
 fi
 
